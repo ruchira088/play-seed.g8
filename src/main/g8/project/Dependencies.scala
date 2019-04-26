b1648d022717fa6ac34cd8eac57541cb2fd07d85
Ruchira (Richie) Jayasekara <ruchira088@gmail.com>
2019-04-26 18:50:00 +1000

    Updating dependencies

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
 
   lazy val jodaTime = "joda-time" % "joda-time" % "2.10.1"
 
-  lazy val scalaTestPlusPlay = "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.1"
+  lazy val scalaTestPlusPlay = "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.2"
   
   lazy val pegdown = "org.pegdown" % "pegdown" % "1.6.0"
 }
