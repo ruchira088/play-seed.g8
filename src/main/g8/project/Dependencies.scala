d60e0836edb05412814de383446f3303a7bd027a
Ruchira (Richie) Jayasekara <ruchira088@gmail.com>
2019-06-28 18:31:31 +1000

    Upgrading scalatestplus-play to 4.0.3

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
 
   lazy val jodaTime = "joda-time" % "joda-time" % "2.10.2"
 
-  lazy val scalaTestPlusPlay = "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.2"
+  lazy val scalaTestPlusPlay = "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.3"
 
   lazy val pegdown = "org.pegdown" % "pegdown" % "1.6.0"
 }
