b366b52c87710856a3e508d8e8a7ed7eebbc9d80
Ruchira (Richie) Jayasekara <ruchira088@gmail.com>
2019-07-09 19:10:50 +1000

    Upgrading the PlayFramework version to 2.7.3

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   val SCALA_VERSION = "2.12.8"
 
-  lazy val jodaTime = "joda-time" % "joda-time" % "2.10.2"
+  lazy val jodaTime = "joda-time" % "joda-time" % "2.10.3"
 
   lazy val scalaTestPlusPlay = "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.3"
 
