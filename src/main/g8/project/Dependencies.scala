c03963a1bf1ce5891bac6605718a143ea1283ec7
Ruchira (Richie) Jayasekara <me@ruchij.com>
2019-12-15 15:03:11 +1100

    Upgrading to Play 2.8

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -3,11 +3,11 @@ import sbt._
 object Dependencies {
   val SCALA_VERSION = "2.13.1"
 
-  lazy val kindProjector = "org.typelevel" %% "kind-projector" % "0.10.3"
+  lazy val kindProjector = "org.typelevel" %% "kind-projector" % "0.11.0" cross CrossVersion.full
 
   lazy val jodaTime = "joda-time" % "joda-time" % "2.10.5"
 
-  lazy val scalaTestPlusPlay = "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.3"
+  lazy val scalaTestPlusPlay = "org.scalatestplus.play" %% "scalatestplus-play" % "5.0.0"
 
   lazy val pegdown = "org.pegdown" % "pegdown" % "1.6.0"
 }
