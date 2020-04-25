5b907888ba12dd8bbbfb500aea8b216a0fe8b8c7
Ruchira (Richie) Jayasekara <me@ruchij.com>
2020-04-25 20:21:12 +1000

    Upgrading joda-time to 2.10.6

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
 
   lazy val scalaTypedHoles = "com.github.cb372" % "scala-typed-holes" % "0.1.3" cross CrossVersion.full
 
-  lazy val jodaTime = "joda-time" % "joda-time" % "2.10.5"
+  lazy val jodaTime = "joda-time" % "joda-time" % "2.10.6"
 
   lazy val scalaTestPlusPlay = "org.scalatestplus.play" %% "scalatestplus-play" % "5.0.0"
 
